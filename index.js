58ce4a0abb6a7ca27ae0b4d72bbf462333a8e3a4
phamus <sphamus@gmail.com>
2020-04-14 23:33:30 +0100

    Dialog flow routes

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+const dialogFlowRoutes = require("./routes/dialogFlowRoutes");
+
+app.use(bodyParser.json());
+
+app.use("/api", dialogFlowRoutes);
 
 app.get("/", (req, res) => {
   res.send({ Hello: "There" });
 });
+
+app.post("/api/df_text_query", (req, res) => {});
+
+app.post("/api/df_event_query", (req, res) => {});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
